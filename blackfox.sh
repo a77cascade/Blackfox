ca2fe1e15f952d1fd93ac47f746094207fb87cdd
Валентин <73306072+a77cascade@users.noreply.github.com>
2020-12-31 07:25:14 +0300

    Update blackfox.sh

--- blackfox.sh
+++ blackfox.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# Arch Linux Fast Install - Быстрая установка Arch Linux https://github.com/ordanax/arch
-# Цель скрипта - быстрое развертывание системы с вашими персональными настройками (конфиг XFCE, темы, программы и т.д.).
-# Автор скрипта Алексей Бойко https://vk.com/ordanax
-
+# Arch Linux Fast Install - Быстрая установка  Linux https://github.com/a77cascade/Blackfox
+# Цель скрипта - быстрое развертывание системы с вашими персональными настройками.
+# Автор скрипта Нестеров Валентин https://vk.com/wellwar
 
 loadkeys ru
 setfont cyr-sun16
 echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
 echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
 
-echo '2.3 Синхронизация системных часов'
+echo '1 Синхронизация системных часов'
 timedatectl set-ntp true
 
-echo '2.4 создание разделов'
+echo '2 Создание разделов'
 (
  echo g;
 
@@ -46,10 +45,10 @@ echo '2.4 создание разделов'
  echo w;
 ) | fdisk /dev/sda
 
-echo 'Ваша разметка диска'
+echo '3 Ваша разметка диска'
 fdisk -l
 
-echo '2.4.2 Форматирование дисков'
+echo '4 Форматирование дисков'
 
 mkfs.fat -F32 /dev/sda1
 mkfs.btrfs  /dev/sda2
@@ -57,25 +56,33 @@ mkswap /dev/sda3
 swapon /dev/sda3
 mkfs.btrfs  /dev/sda4
 
-echo '2.4.3 Монтирование дисков'
+echo '5 Монтирование дисков'
 mount /dev/sda2 /mnt
 mkdir /mnt/home
 mkdir -p /mnt/boot/efi
 mount /dev/sda1 /mnt/boot/efi
 mount /dev/sda4 /mnt/home
 
-echo '3.1 Выбор зеркал для загрузки.'
+echo '6 Выбор зеркал для загрузки.'
 pacman -S reflector
 reflector —verbose -l 200 —sort rate —save /etc/pacman.d/mirrorlist
-curl -O https://blackarch.org/strap.sh
-bash ./strap.sh	
-pacman -Ss blackarch-mirrorlist
-pacman -Syyu
 
-echo '3.2 Установка основных пакетов'
+echo '7 Установить репозитории Blackarch?'
+read -p "1 - Да, 2 - Нет: " ba
+if [[ $ba == 1 ]]; then
+    curl -O https://blackarch.org/strap.sh
+    bash ./strap.sh	
+    pacman -Ss blackarch-mirrorlist
+    pacman -Syyu
+elif [[ $ba == 2 ]]; then
+  echo '7.1 Пропускаем.'
+  pacman -Syyu
+fi
+
+echo '8 Установка основных пакетов'
 pacstrap /mnt base base-devel bash-completion linux-hardened linux-firmware nano dhcpcd netctl
 
-echo '3.3 Настройка системы'
+echo '9 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt sh -c "$(curl -fsSL git.io/blackfox2.sh)"
