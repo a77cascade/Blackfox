e9e023da5950b4e270999115eb8856d91dd70113
Валентин <73306072+a77cascade@users.noreply.github.com>
2020-12-31 07:50:34 +0300

    Update blackfox2.sh

--- blackfox2.sh
+++ blackfox2.sh
@@ -1,78 +1,78 @@
 #!/bin/bash
-read -p "Введите имя компьютера: " hostname
-read -p "Введите имя пользователя: " username
+read -p '10 Введите имя компьютера: ' hostname
+read -p '11 Введите имя пользователя: ' username
 
-echo 'Прописываем имя компьютера'
+echo '12 Прописываем имя компьютера'
 echo $hostname > /etc/hostname
 ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
-echo '3.4 Добавляем русскую локаль системы'
+echo '13 Добавляем русскую локаль системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
 
-echo 'Обновим текущую локаль системы'
+echo '14 Обновим текущую локаль системы'
 locale-gen
 
-echo 'Указываем язык системы'
+echo '15 Указываем язык системы'
 echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 
-echo 'Вписываем KEYMAP=ru FONT=cyr-sun16'
+echo '16 Вписываем KEYMAP=ru FONT=cyr-sun16'
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-echo 'Создадим загрузочный RAM диск'
+echo '17 Создадим загрузочный RAM диск'
 mkinitcpio -p linux
 
-echo '3.5 Устанавливаем загрузчик'
+echo '18 Устанавливаем загрузчик'
 pacman -Syy
 pacman -S grub efibootmgr --noconfirm 
 grub-install /dev/sda
 
-echo 'Обновляем grub.cfg'
+echo '19 Обновляем grub.cfg'
 grub-mkconfig -o /boot/grub/grub.cfg
 
-echo 'Ставим программу для Wi-fi'
+echo '20 Ставим программу для Wi-fi'
 pacman -S dialog wpa_supplicant --noconfirm 
 
-echo 'Добавляем пользователя'
+echo '21 Добавляем пользователя'
 useradd -m -g users -G wheel -s /bin/bash $username
 
-echo 'Создаем root пароль'
+echo '22 Создаем root пароль'
 passwd
 
-echo 'Устанавливаем пароль пользователя'
+echo '23 Устанавливаем пароль пользователя'
 passwd $username
 
-echo 'Устанавливаем SUDO'
+echo '24 Устанавливаем SUDO'
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
-echo 'Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
+echo '25 Раскомментируем репозиторий multilib Для работы 32-битных приложений в 64-битной системе.'
 echo '[multilib]' >> /etc/pacman.conf
 echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
 pacman -Syy
 
-echo 'Ставим иксы и драйвера'
+echo '26 Ставим иксы и драйвера'
 pacman -S $gui_install
 
-echo "Ставим Cinnamon и Gnome"
+echo "27 Ставим Cinnamon и Gnome"
 pacman -S xorg-server xorg-drivers xorg-xinit xorg-apps mesa xorg-twm xorg-xclock gdm cinnamon gnome gnome-tweaks gnome-calculator gnome-screenshot gnome-shell-extensions gnome-catgen-git gnome-extra --noconfirm
 
-echo 'Cтавим DM'
+echo '28 Cтавим DM'
 pacman -S gdm --noconfirm
 mv /usr/share/xsessions/gnome.desktop ~/
 systemctl enable gdm
 
-echo 'Ставим шрифты'
+echo '29 Ставим шрифты'
 pacman -S ttf-liberation ttf-dejavu --noconfirm
 
-echo 'Ставим сеть'
+echo '30 Ставим сеть'
 pacman -S networkmanager network-manager-applet ppp --noconfirm
 
-echo 'Подключаем автозагрузку менеджера входа и интернет'
+echo '31 Подключаем автозагрузку менеджера входа и интернет'
 systemctl enable NetworkManager
 
-echo 'Установка wget git asp'
+echo '32 Установка wget git asp'
 pacman -S wget git asp
 
-echo 'Установка подпрограм'
+echo '33 Установка подпрограм'
 wget git.io/blackfox3.sh && sh blackfox3.sh
