80be382289371d5a8987bccd8cb558c8abba7c21
Валентин <73306072+a77cascade@users.noreply.github.com>
2021-01-20 12:29:07 +0300

    Update blackfox3.sh

--- blackfox3.sh
+++ blackfox3.sh
@@ -7,7 +7,7 @@ sudo pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
 
 echo '36 Установка базовых программ и пакетов'
-sudo pacman -S neofetch chromium veracrypt vlc kdenlive qbittorrent torbrowser-launcher pulseaudio pulseaudio-alsa pavucontrol pcmanfm terminology --noconfirm
+sudo pacman -S neofetch chromium veracrypt vlc kdenlive qbittorrent torbrowser-launcher pulseaudio pulseaudio-alsa alsa-utils alsa-oss pavucontrol pcmanfm terminology
 
 echo '38 Добавить репозитории Blackarch?'
 wget https://blackarch.org/strap.sh
@@ -25,4 +25,4 @@ echo 'Обновления системы и чистка её'
 pacman -Sy linux
 
 echo '43 Установка завершена!'
-sudo reboot
+exit
