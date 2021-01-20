33509ee889b12b8d307661a6076f00ba3d52f716
Валентин <73306072+a77cascade@users.noreply.github.com>
2021-01-20 08:08:26 +0300

    Update blackfox2.sh

--- blackfox2.sh
+++ blackfox2.sh
@@ -20,9 +20,6 @@ echo '16 Вписываем KEYMAP=ru FONT=cyr-sun16'
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-echo '17 Создадим загрузочный RAM диск'
-mkinitcpio -p linux
-
 echo '18 Устанавливаем загрузчик'
 pacman -Syy
 pacman -S grub efibootmgr  
@@ -56,12 +53,12 @@ echo '26 Ставим иксы и драйвера'
 pacman -S xorg-server xorg-drivers xorg-xinit xorg-apps mesa xorg-twm xorg-xclock xorg xf86-input-synaptics lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader intel-ucode iucode-tool broadcom-wl --noconfirm
 
 echo "27 Ставим Gnome"
-pacman -S gnome gnome-tweaks-tool
+pacman -S awesome
 
 echo '28 Cтавим DM'
-pacman -S gdm 
-mv /usr/share/xsessions/gnome.desktop ~/
-systemctl enable gdm
+pacman -S lxdm
+cp /etc/lxdm/lxdm.conf ~/.condig/lxdm.conf
+systemctl enable lxdm
 
 echo '29 Ставим шрифты'
 pacman -S ttf-liberation ttf-dejavu 
@@ -73,7 +70,14 @@ echo '31 Подключаем автозагрузку менеджера вхо
 systemctl enable NetworkManager
 
 echo '32 Установка wget git asp'
-pacman -S wget git asp
+pacman -S git
+sudo pacman -S --noconfirm --needed wget curl asp
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+# makepkg -si
+makepkg -si --skipinteg
+cd ..
+rm -rf yay-bin
 
 echo '33 Установка подпрограм'
 wget git.io/blackfox3.sh && sh blackfox3.sh
